4698b0ff944ff67256da4578861238e482a8d12f
emouisaac <82849663+emouisaac@users.noreply.github.com>
2025-06-05 08:41:00 +0300

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,39 @@
+<?php
+$name = $_POST['name'] ?? '';
+$visitor_email= $_POST['email'] ?? '';
+$subject = $_POST['subject'] ?? '';
+$message = $_POST['message'] ?? '';
+
+
+$email_from = 'info@coinacademia.in'; // Replace with your email address
+$email_subject = "New Form Submission: $subject";
+$email_body = "You have received a new message from the user $name.\n".
+              "Here are the details:\n".
+              "Name: $name\n".
+              "Email: $visitor_email\n".
+              "Subject: $subject\n".
+              "Message: $message\n";
+$to = 'emouisaac1@gmail.com'; // Replace with your email address
+$headers = "From: $email_from\r\n";
+$headers .= "Reply-To: $visitor_email\r\n";
+mail($to, $email_subject, $email_body, $headers);
+if(!isset($_POST['submit'])) {
+    echo "Form not submitted correctly.";
+    exit;
+}
+// Check if the form fields are empty
+header("Location: ../index.html"); // Redirect to the homepage after submission
+if(empty($name) || empty($visitor_email) || empty($subject) || empty($message)) {
+    echo "All fields are required.";
+    exit;
+}
+
+
+
+if(mail($to, $email_subject, $email_body, $headers)) {
+    echo "Email sent successfully.";
+} else {
+    echo "Failed to send email.";
+}
+
+?>
