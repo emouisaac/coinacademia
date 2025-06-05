74d4e187b93f8e4a7bca43a6b93130fd3ebc9430
emouisaac <82849663+emouisaac@users.noreply.github.com>
2025-06-05 12:48:54 +0300

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,24 +1,20 @@
 <?php
 $name = $_POST['name'] ?? '';
-$visitor_email= $_POST['email'] ?? '';
+$visitor_email = $_POST['email'] ?? '';
 $subject = $_POST['subject'] ?? '';
 $message = $_POST['message'] ?? '';
 
-
 $email_from = 'info@coinacademia.in';
-$email_subject = "New Form Submission: $subject";
-$email_body = "You have received a new message from the user $name.\n".
-              "Here are the details:\n".
-              "Name: $name\n".
-              "Email: $visitor_email\n".
-              "Subject: $subject\n".
-              "Message: $message\n";
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name.\n" .
+              "User Email: $visitor_email.\n" .
+              "Subject: $subject.\n" .
+              "User Message: $message.\n";
+
 $to = 'emouisaac1@gmail.com';
 $headers = "From: $email_from\r\n";
 $headers .= "Reply-To: $visitor_email\r\n";
+
 mail($to, $email_subject, $email_body, $headers);
-// Check if the form fields are empty
-header("Location: ../index.html");
-
-
+header("Location: DCP.html?mailsent=true");
 ?>
