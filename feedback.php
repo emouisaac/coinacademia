239c0f593a39548f24ccd8a83d056755844273a0
emouisaac <82849663+emouisaac@users.noreply.github.com>
2025-06-06 09:13:48 +0300

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,14 +1,16 @@
 <?php
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailForm = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+if (isset($_GET['submit'])) {
+    $name = $_GET['name'];
+    $mailForm = $_GET['email'];
+    $subject = $_GET['subject'];
+    $message = $_GET['message'];
 
     $mailTo = "info@coinacademia.in";
     $headers = "From: ".$mailForm;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.php?mailsent=true");
 }
+?>
+<!DOCTYPE html>
